4b969922074d647abe623e67a49f3037bb5a5e09
Tyler Sebastian <tills13@gmail.com>
2017-03-02 09:12:14 -0800

    remove spaces

--- app/utils.py
+++ app/utils.py
@@ -8,19 +8,15 @@ def timing(label):
     elapsed = time.time() - start_time
     print('{} took {}ms'.format(label, elapsed * 1000))
 
-
 def add(a, b):
     return (a[0] + b[0], a[1] + b[1])
-
 
 def sub(a, b):
     return (a[0] - b[0], a[1] - b[1])
 
-
 def dist(a, b):
     """ Returns the 'manhattan distance' between a and b """
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
 
 def neighbours(pos):
     # Inlined for performance:
